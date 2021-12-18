66e0bfec2b8d03e5667b258616999c9851d83120
Eric Le Van Quyen <eric.levanquyen@gmail.com>
2021-12-18 10:29:59 +0100

    done

---
+++ eric-setup.sh
@@ -0,0 +1,27 @@
+
+
+sudo pacman -S sakura
+
+sudo pacman -S fish
+
+chsh -s /usr/bin/fish
+
+sudo pacman -S exa
+
+sudo pacman -S starship
+
+#fish
+
+sudo pacman -S paru
+
+sudo pacman -S broot
+
+sudo cp ~/dotfiles/paru.conf /etc/paru.conf
+
+
+sudo pacman -S plocate
+
+sudo pacman -S the_platinum_searcher-bin
+
+
+sudo cp ~/dotfiles/sddm.conf /etc/sddm.conf
---
+++ scripts/backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+#rclone sync homenas:../../srv/dev-disk-by-uuid-9a2f5451-cec1-4203-a09a-04abccd0b953/Music  /Media/Music/  -P && echo -e "TEST HEURE FIXE Synchro de Music effectuée (Salon -> Bureau) !!!"
+#rclone sync /Media/TVShows  homenas:../../srv/dev-disk-by-uuid-9a2f5451-cec1-4203-a09a-04abccd0b953/TV-Shows  -P && echo -e "TEST HEURE FIXE Synchro de TVshows effectuée (Bureau -> Salon) !!!"
+#rclone sync /Media/Movies  homenas:../../srv/dev-disk-by-uuid-9a2f5451-cec1-4203-a09a-04abccd0b953/Movies  -P && echo -e "TEST HEURE FIXE Synchro de Movies effectuée (Bureau -> Salon) !!!"
+echo -e "TEST HEURE FIXE Synchro de Music effectuée (Salon -> Bureau) !!!"
